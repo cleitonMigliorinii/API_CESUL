24c8de3e85303136e4298531f115b05d5060544d
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2023-11-01 19:20:34 -0300

    projeto

---
+++ pedidoCesul/src/main/java/br/cesul/pedidoCesul/vendedor/IVendedorRepository.java
@@ -0,0 +1,11 @@
+package br.cesul.pedidoCesul.vendedor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IVendedorRepository extends JpaRepository<VendedorModel, UUID> {
+   VendedorModel findByEmail(String email);
+   List<VendedorModel> findAllBySituacao(boolean situacao);
+}
