a212269bbc9fe3b0cebd1585aaef9e468b24f208
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2024-04-22 17:53:02 -0300

    InheritanceType.TABLE_PER_CLASS

--- pedidoCesul/src/main/java/br/cesul/pedidoCesul/pessoa/Pessoa.java
+++ pedidoCesul/src/main/java/br/cesul/pedidoCesul/pessoa/Pessoa.java
@@ -7,9 +7,8 @@ import java.util.UUID;
 
 @Entity
 @Data
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "tipo_pessoa", discriminatorType = DiscriminatorType.STRING)
-public class Pessoa {
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Pessoa {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID uuid;
